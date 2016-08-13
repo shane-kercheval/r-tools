08788c612432ea9acd48279ce9388fb522536e02
shane-kercheval <shanekercheval@Shanes-MacBook-Pro.local>
2016-08-12 20:57:33 -0700

    bug

--- tests/test_basic_stats.R
+++ tests/test_basic_stats.R
@@ -8,7 +8,7 @@ source('../general/basic_stats.R', chdir=TRUE)
 #test_file("test_basic_stats.R")
 
 test_that("general: basic_stats", {
-	vector = seq(from=10, to=100, by=1)
+	vector = seq(from=0, to=100, by=1)
 
 	percentile_matrix = create_percentile_matrix(list_of_datasets=list(vector), 'test')
 	expect_true(all(percentile_matrix[1,1:13] == c(0, 0.025, 0.05, 0.10, 0.25, 0.50, 0.75, 0.90, 0.95, 0.975, 1, -0.5, 1.5)*100))

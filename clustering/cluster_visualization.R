d1b39ae985c163a79732a03f14e062bea1f68df7
shane-kercheval <shanekercheval@Shanes-MacBook-Pro.local>
2017-01-25 21:02:17 -0800

    refactor

--- clustering/cluster_visualization.R
+++ clustering/cluster_visualization.R
@@ -36,7 +36,7 @@ save_hierarchical_heatmaps <- function(hierarchical_results, folder='', subscrip
 		subscript = sprintf('_%s', subscript)
 	}
 
-	temp = lapply(hierarchical_results, FUN=function(hierarchical_result){
+	heatmaps = lapply(hierarchical_results, FUN=function(hierarchical_result){
 		results_df = as.data.frame(lapply(hierarchical_result, colMeans))
 		results_df = as.data.frame(t(results_df)) # t (transpose) results in a matrix, convert back to df
 		results_df$cluster_name = 1:nrow(results_df)
@@ -45,9 +45,11 @@ save_hierarchical_heatmaps <- function(hierarchical_results, folder='', subscrip
 		{
 			ggsave(filename=sprintf("./%s/hierarchical%s_%s_clusters_%s.png", folder, subscript, length(hierarchical_result), Sys.Date()), plot=heatmap_plot)
 		}
+
+		return(heatmap_plot)
 	})
 
-	return (temp)
+	return (heatmaps)
 }
 
 hierarchical_dendogram <-function(data_frame, named_column, ideal_cluster_size=NULL, path='./dendogram.png')
